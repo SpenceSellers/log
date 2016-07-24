f7cf16b292dc9486e15a54a510f7144bfec4f9bc
Spencer Sellers <spencesellers@gmail.com>
2016-07-24 12:08:47 -0500

    Moved Vec<Entry> stuff into a Journal struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::Write;
 use std::fmt;
+use std::fmt::Write;
+
 
 extern crate time;
 use time::Tm;
@@ -15,40 +16,57 @@ struct Entry {
 }
 
 impl Entry {
-    fn write<W: Write> (&self, sink: &mut W) -> Result<(), std::io::Error> {
+    fn encoded(&self) -> String {
         let time_s = time::strftime("%F", &self.date).expect("Failed date conversion???");
-        write!(sink, "**| {} @{}:\n{}\n\n", time_s, self.group, self.content)
+        let mut sink = String::new();
+        write!(&mut sink, "**| {} @{}:\n{}\n\n", time_s, self.group, self.content).unwrap();
+        return sink;
     }
 }
 
 impl fmt::Display for Entry {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut s = String::new();
-        self.write(&mut s);
-        write!(f, "{}", s)
+        write!(f, "{}", self.encoded())
+    }
+}
+
+struct Journal {
+    entries: Vec<Entry>
+}
+
+impl Journal {
+    fn from_entries(v: Vec<Entry>) -> Self {
+        Journal {entries: v}
+    }
+
+    fn from_str(s: &str) -> Option<Journal> {
+        let re = Regex::new(r"\*\*\|\s+(\d{4}-\d{2}-\d{2})\s+@(\w+)\s+(.*)").unwrap();
+        let caps = re.captures_iter(s);
+
+        let mut entries = Vec::new();
+        for cap in caps {
+            let entry = Entry {
+                date: parse_date(cap.at(1).unwrap()).expect("Bad date!"),
+                group: cap.at(2).unwrap().to_string(),
+                content: cap.at(3).unwrap().to_string()
+            };
+            entries.push(entry);
+        }
+        return Some(Journal::from_entries(entries));
+    }
+
+    fn encoded(&self) -> String {
+        let mut s = String::new();
+        for entry in &self.entries {
+            s.push_str(&entry.encoded());
+        }
+        return s;
     }
 }
 
 fn parse_date(s: &str) -> Option<Tm> {
     time::strptime(s, "%F").ok()
-}
-
-fn parse_entries(s: &str) -> Option<Vec<Entry>> {
-    let re = Regex::new(r"\*\*\|\s+(\d{4}-\d{2}-\d{2})\s+@(\w+)\s+(.*)").unwrap();
-    let caps = re.captures_iter(s);
-
-    let mut entries = Vec::new();
-    for cap in caps {
-        let entry = Entry {
-            date: parse_date(cap.at(1).unwrap()).expect("Bad date!"),
-            group: cap.at(2).unwrap().to_string(),
-            content: cap.at(3).unwrap().to_string()
-        };
-        entries.push(entry);
-    }
-
-    return Some(entries);
-
 }
 
 
@@ -61,8 +79,10 @@ fn main() {
     //
     // e.write(&mut std::io::stdout());
 
-    parse_entries(
+    let journal = Journal::from_str(
         "**| 2016-05-20 @prog
         U SUX
-        **| 2016-07-10 @general ALSO SUX");
+        **| 2016-07-10 @general ALSO SUX").unwrap();
+
+    println!("{}", journal.encoded());
 }
