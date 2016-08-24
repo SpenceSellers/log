0dcfa0f6a498a8883b4bd8eb2f18606f6f23814d
Spencer Sellers <spencesellers@gmail.com>
2016-08-24 00:11:40 -0500

    Removed unneeded lifetime

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 
 
-pub fn shallow_copy<'a, T> (source: &'a Vec<T>) -> Vec<&'a T> {
+pub fn shallow_copy<T> (source: &Vec<T>) -> Vec<&T> {
     let mut v = Vec::new();
     for item in source {
         v.push(item);
